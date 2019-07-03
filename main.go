a4abdaa13f97e2c683ed943c2a014237ca9a9bbd
Junhyeok99 <apple2870907@gmail.com>
2019-07-03 12:29:12 +0900

    Add Mux

--- main.go
+++ main.go
@@ -1,20 +1,49 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
+
+type Article struct {
+	Title string `json:"Title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title:"Test Title", Desc:"Test Description", Content:"Hello, World!"},
+	}
+
+	fmt.Println("Endpoint Hit: All Articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
+
+func testPostArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Test POST endpoint worked")
+}
 
 func homepage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Homepage")
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homepage)
-	log.Fatal(http.ListenAndServe(":1423", nil))
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homepage)
+	myRouter.HandleFunc("/articles", allArticles).Methods(http.MethodGet)
+	myRouter.HandleFunc("/articles", testPostArticles).Methods(http.MethodPost)
+	log.Fatal(http.ListenAndServe(":1423", myRouter))
 }
 
 func main() {
+	fmt.Println("Serve on http://localhost:1423")
 	handleRequests()
 }
